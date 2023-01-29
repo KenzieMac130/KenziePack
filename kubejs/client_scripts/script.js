167fe0dc71dd0bb678846b655ba5a13886dad52c
KenzieMac130 <kenziemac130@protonmail.com>
2023-01-28 20:34:12 -0500

    Massive Update

---
+++ kubejs/client_scripts/script.js
@@ -0,0 +1,14 @@
+// priority: 0
+
+REIEvents.hide('item', event => {
+	event.hide("campanion:rope")
+	event.hide("additionaladditions:rope")
+	event.hide("campanion:rope_bridge_planks")
+	event.hide("campanion:rope_bridge_post")
+	event.hide("bewitchment:waystone")
+	event.hide("terrarianslimes:glowstick")
+	event.hide("terrarianslimes:bouncy_glowstick")
+	event.hide("terrarianslimes:sticky_glowstick")
+	event.hide("ad_astra:hammer")
+	event.hide("ad_astra:iron_plate")
+})
---
+++ kubejs/server_scripts/script.js
@@ -0,0 +1,164 @@
+// priority: 0
+
+ServerEvents.recipes(event => {
+	// Change recipes here
+	event.replaceInput({}, 'campanion:rope', 'farmersdelight:rope')
+	event.replaceInput({}, 'additionaladditions:rope', 'farmersdelight:rope')
+	event.remove({ output: 'campanion:rope' })
+	event.remove({ output: 'additionaladditions:rope' })
+	event.remove({ output: 'campanion:rope_bridge_planks' })
+	event.remove({ output: 'campanion:rope_bridge_post' })
+	event.remove({ output: "terrarianslimes:glowstick" })
+	event.remove({ output: "terrarianslimes:bouncy_glowstick" })
+	event.remove({ output: "terrarianslimes:sticky_glowstick" })
+	event.remove({ output: "ad_astra:hammer" })
+	event.remove({ input: "ad_astra:hammer" })
+	event.remove({ input: "ad_astra:iron_plate" })
+	event.remove({ output: "ad_astra:iron_plate" })
+
+	event.remove({ output: 'computercraft:computer_normal' })
+	event.remove({ output: 'computercraft:pocket_computer_normal' })
+	event.remove({ output: 'computercraft:monitor_normal' })
+	event.remove({ output: 'computercraft:disk_drive' })
+	event.remove({ output: 'computercraft:wired_modem' })
+	event.remove({ output: 'computercraft:printer' })
+	event.remove({ output: 'computercraft:cable' })
+	event.remove({ output: 'ad_astra:nasa_workbench' })
+
+	event.shaped(
+		Item.of('computercraft:computer_normal', 1),
+		[
+			'AAA',
+			'ABA',
+			'ACA'
+		],
+		{
+			A: '#c:stones',
+			B: 'minecraft:redstone',
+			C: 'minecraft:echo_shard'
+		}
+	)
+
+	event.shaped(
+		Item.of('computercraft:pocket_computer_normal', 1),
+		[
+			'AAA',
+			'ABA',
+			'ACA'
+		],
+		{
+			A: '#c:stones',
+			B: 'minecraft:golden_apple',
+			C: 'minecraft:echo_shard'
+		}
+	)
+
+	event.shaped(
+		Item.of('computercraft:monitor_normal', 1),
+		[
+			'AAA',
+			'ABA',
+			'AAA'
+		],
+		{
+			A: '#c:stones',
+			B: 'minecraft:echo_shard'
+		}
+	)
+
+	event.shaped(
+		Item.of('computercraft:disk_drive', 1),
+		[
+			'AAA',
+			'ABA',
+			'AAA'
+		],
+		{
+			A: '#c:stones',
+			B: 'minecraft:jukebox'
+		}
+	)
+
+	event.shaped(
+		Item.of('computercraft:wired_modem', 1),
+		[
+			'AAA',
+			'ABA',
+			'AAA'
+		],
+		{
+			A: '#c:stones',
+			B: 'createaddition:copper_wire'
+		}
+	)
+
+	event.shaped(
+		Item.of('computercraft:printer', 1),
+		[
+			'AAA',
+			'ABA',
+			'AAA'
+		],
+		{
+			A: '#c:stones',
+			B: 'create:mechanical_press'
+		}
+	)
+
+	event.shapeless(
+		Item.of('computercraft:cable', 6),
+		[
+			'createaddition:copper_wire',
+			'minecraft:stone'
+		]
+	)
+
+	event.shaped(
+		Item.of('ad_astra:nasa_workbench', 1),
+		[
+			'EDE',
+			'ACA',
+			'ABA'
+		],
+		{
+			A: 'minecraft:redstone_block',
+			B: 'ad_astra:steel_block',
+			C: 'computercraft:computer_normal',
+			D: 'minecraft:elytra',
+			E: 'minecraft:redstone_torch'
+		}
+	)
+
+	event.recipes.createMixing({
+		ingredients: [
+			{
+				item: "minecraft:fire_charge"
+			},
+			{
+				item: "minecraft:ink_sac"
+			}
+		],
+		results: [
+			{
+				fluid: "ad_astra:oil",
+				amount: 81000
+			}
+		]
+	})
+})
+
+// ServerEvents.tags('item', event => {
+// 	// Get the #forge:cobblestone tag collection and add Diamond Ore to it
+// 	// event.get('forge:cobblestone').add('minecraft:diamond_ore')
+
+// 	// Get the #forge:cobblestone tag collection and remove Mossy Cobblestone from it
+// 	// event.get('forge:cobblestone').remove('minecraft:mossy_cobblestone')
+// })
+
+LootJS.modifiers(event => {
+	event.addLootTableModifier(/.*/).replaceLoot("campanion:rope", "farmersdelight:rope")
+	event.addLootTableModifier(/.*/).replaceLoot("additionaladditions:rope", "farmersdelight:rope")
+	event.addLootTableModifier(/.*/).replaceLoot("bewitchment:waystone", "waystones:warp_stone")
+	event.addLootTableModifier(/.*/).replaceLoot("ad_astra:iron_plate", "create:iron_sheet")
+	event.addLootTableModifier(/.*/).removeLoot("ad_astra:hammer")
+})
